9f1c19393e48a57ee3a9d933d6e558f2d6b8cf87
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-06 19:54:31 +0200

    Implement Run method

--- xpd.go
+++ xpd.go
@@ -14,3 +14,55 @@ type Feed struct {
 type FeedReader interface {
 	GetNewPosts() []Post
 }
+
+type Detector interface {
+	findSuspects(Post, []Feed) []Post
+}
+
+type Listener interface {
+	onSuspect(Post, []Post)
+}
+
+type Context struct {
+	feeds     []Feed
+	readers   []FeedReader
+	detectors []Detector
+	listeners []Listener
+}
+
+func Run(configfile string) {
+	context := readContext()
+
+	posts := make(chan Post)
+
+	for _, reader := range (context.readers) {
+		go waitForPosts(reader, posts)
+	}
+
+	for {
+		processQueue(context, posts)
+	}
+}
+
+func readContext() Context {
+	// TODO read configuration and create context elements
+	return Context{}
+}
+
+func waitForPosts(reader FeedReader, posts chan<- Post) {
+	for {
+		for _, post := range (reader.GetNewPosts()) {
+			posts <- post
+		}
+	}
+}
+
+func processQueue(context Context, posts chan Post) {
+	post := <-posts
+	for _, detector := range (context.detectors) {
+		suspects := detector.findSuspects(post, context.feeds)
+		for _, listener := range (context.listeners) {
+			listener.onSuspect(post, suspects)
+		}
+	}
+}
