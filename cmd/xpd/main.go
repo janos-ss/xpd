562d2e04458916a98e55e663ede73545642a817a
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-06 19:48:11 +0200

    Add cli

---
+++ cmd/xpd/main.go
@@ -0,0 +1,33 @@
+// Command line interface to run Cross Post Detector
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"github.com/janos-ss/xpd"
+)
+
+const defaultConfigFile = "xpd.conf"
+
+type Params struct {
+	configfile string
+}
+
+func parseArgs() Params {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [options]\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	config := flag.String("config", defaultConfigFile, "path to configuration file")
+	flag.Parse()
+
+	return Params{config}
+}
+
+func main() {
+	params := parseArgs()
+
+	xpd.Run(params.configfile)
+}
