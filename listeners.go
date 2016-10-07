76b979addeea69e5f7325e2d74fd68a11d6f1a54
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-07 10:43:06 +0200

    Make consolePrinterListener log instead of print

--- listeners.go
+++ listeners.go
@@ -1,13 +1,15 @@
 package xpd
 
-import "fmt"
+import (
+	"log"
+)
 
 type consolePrinterListener struct{}
 
 func (listener consolePrinterListener) onDuplicates(post Post, oldPosts []Post) {
-	fmt.Printf("possible cross-post: %s (%s)\n", post.Subject, post.Id)
-	for _, oldPost := range(oldPosts) {
-		fmt.Printf("  of: %s (%s)\n", oldPost.Subject, oldPost.Id)
+	log.Printf("possible cross-post: %s (%s)\n", post.Subject, post.Id)
+	for _, oldPost := range (oldPosts) {
+		log.Printf("  of: %s (%s)\n", oldPost.Subject, oldPost.Id)
 	}
-	fmt.Println()
+	log.Println("----")
 }
