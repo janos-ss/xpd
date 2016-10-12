b03644c1f4cc6c4f5fabf7b84b683f3eeeea57b4
Janos Gyerik <janos@kronos>
2016-10-12 23:33:22 +0200

    Move tests on detectors to dedicated file

---
+++ detectors_test.go
@@ -0,0 +1,115 @@
+package xpd
+
+import (
+	"testing"
+	"reflect"
+)
+
+func Test_SameBodyDetector_FindDuplicates_finds_same_body(t *testing.T) {
+	body := "some text"
+	differentBody := body + " blah"
+
+	post := Post{Body: body}
+
+	var repo PostRepository = NewPostRepository()
+	repo.Add(post)
+	repo.Add(Post{Body: differentBody})
+
+	var detector Detector = SameBodyDetector{}
+	if !reflect.DeepEqual(detector.FindDuplicates(post, []Post{post}), []Post{post}) {
+		t.Errorf("same-body-detector should find only the match")
+	}
+}
+
+func Test_splitToWords(t *testing.T) {
+	s := "   @#$@hello THERE 4324%%%$# ouch  "
+	if actual, expected := splitToWords(s), []string{"hello", "there", "ouch"}; !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %s; expected %s", actual, expected)
+	}
+}
+
+func Test_wordCounts(t *testing.T) {
+	s := "Hello World hello again"
+
+	expected := wordCountMap{
+		"hello": 2,
+		"world": 1,
+		"again": 1,
+	}
+	expectedTotal := 4
+
+	if actual, actualTotal := calcWordCounts(s); !reflect.DeepEqual(actual, expected) || actualTotal != expectedTotal {
+		t.Fatalf("got %v, %d; expected %v, %d", actual, actualTotal, expected, expectedTotal)
+	}
+}
+
+func Test_similarEnoughCounts(t *testing.T) {
+	limitRatio := 0.1
+	base := 123
+	if other := base; !similarCounts(base, other, limitRatio) {
+		t.Errorf("got %d and %d are _not_ similar enough, but should be", base, other)
+	}
+	if other := base + calcRatio(base, limitRatio); !similarCounts(base, other, limitRatio) {
+		t.Errorf("got %d and %d are _not_ similar enough, but should be", base, other)
+	}
+	if other := base - calcRatio(base, limitRatio); !similarCounts(base, other, limitRatio) {
+		t.Errorf("got %d and %d are _not_ similar enough, but should be", base, other)
+	}
+	if other := base + calcRatio(base, 1.1*limitRatio); similarCounts(base, other, limitRatio) {
+		t.Errorf("got %d and %d are similar enough, but should _not_ be", base, other)
+	}
+	if other := base - calcRatio(base, 1.1*limitRatio); similarCounts(base, other, limitRatio) {
+		t.Errorf("got %d and %d are similar enough, but should _not_ be", base, other)
+	}
+}
+
+func Test_wordCountDiffs(t *testing.T) {
+	first := wordCountMap{
+		"hello": 7,
+		"world": 13,
+		"again": 17,
+	}
+	second := wordCountMap{
+		"welcome": 23,
+		"new":     29,
+		"world":   31,
+	}
+
+	expectedDiffsLeft := float64(7+17) + float64(31-13)/2
+	expectedDiffsRight := float64(23+29) + float64(31-13)/2
+
+	if actual := calcWordCountDiffs(first, second); actual != expectedDiffsLeft {
+		t.Errorf("got %f; expected %f", actual, expectedDiffsLeft)
+	}
+
+	if actual := calcWordCountDiffs(second, first); actual != expectedDiffsRight {
+		t.Errorf("got %f; expected %f", actual, expectedDiffsRight)
+	}
+}
+
+func Test_SimilarWordCountDetector_with_rearranged_words(t *testing.T) {
+	post := Post{Body: "The quick brown fox jumps over the lazy dog"}
+	rearranged := []Post{Post{Body: "the lazy dog The quick brown fox jumps over"}}
+
+	if !reflect.DeepEqual(SimilarWordCountDetector{}.FindDuplicates(post, rearranged), rearranged) {
+		t.Errorf("got '%v' not a duplicate of '%v', but it should be", rearranged[0].Body, post.Body)
+	}
+}
+
+func Test_SimilarWordCountDetector_with_deleted_words(t *testing.T) {
+	post := Post{Body: "The quick brown fox jumps over the lazy dog filler filler"}
+	deleted := []Post{Post{Body: "The quick brown fox over the lazy dog filler filler"}}
+
+	if !reflect.DeepEqual(SimilarWordCountDetector{}.FindDuplicates(post, deleted), deleted) {
+		t.Errorf("got '%v' not a duplicate of '%v', but it should be", deleted[0].Body, post.Body)
+	}
+}
+
+func Test_SimilarWordCountDetector_with_added_words(t *testing.T) {
+	post := Post{Body: "The quick brown fox jumps over the lazy dog filler filler"}
+	added := []Post{Post{Body: "The quick brown fox jumps over the dumb lazy dog filler filler"}}
+
+	if !reflect.DeepEqual(SimilarWordCountDetector{}.FindDuplicates(post, added), added) {
+		t.Errorf("got '%v' not a duplicate of '%v', but it should be", added[0].Body, post.Body)
+	}
+}
