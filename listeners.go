ba052258153880f06094c61a673e6e76284bf51c
Janos Gyerik <janos@kronos>
2016-10-22 07:56:07 +0200

    Rename formatAsEmail to summary

--- listeners.go
+++ listeners.go
@@ -14,11 +14,7 @@ func (listener ConsolePrinterListener) OnDuplicates(post Post, oldPosts []Post) 
 	defer ct.ResetColor()
 	ct.ChangeColor(ct.Red, true, ct.None, false)
 
-	log.Printf("%s: possible cross-post on: %s (%s)", post.Feed.Id, post.Subject, post.Id)
-	for _, oldPost := range oldPosts {
-		log.Printf("  of: (%s:) %s (%s)", oldPost.Feed.Id, oldPost.Subject, oldPost.Id)
-	}
-	log.Println("------------------------------------------------------------")
+	log.Printf("possible cross-post(s):\n%s", summaryOfDups(post, oldPosts))
 }
 
 type MailerListener struct {
@@ -26,7 +22,7 @@ type MailerListener struct {
 }
 
 func (listener MailerListener) OnDuplicates(post Post, oldPosts []Post) {
-	message := formatAsEmail(post, oldPosts)
+	message := summaryOfDups(post, oldPosts)
 
 	err := listener.Mailer.Send(message)
 	if err != nil {
@@ -34,10 +30,14 @@ func (listener MailerListener) OnDuplicates(post Post, oldPosts []Post) {
 	}
 }
 
-func formatAsEmail(post Post, oldPosts []Post) string {
-	message := fmt.Sprintf("%s: possible cross-post on: %s (%s)\n", post.Feed.Id, post.Subject, post.Id)
-	for _, oldPost := range oldPosts {
-		message += fmt.Sprintf("  of: (%s:) %s (%s)\n", oldPost.Feed.Id, oldPost.Subject, oldPost.Id)
+func summaryOfDups(post Post, oldPosts []Post) string {
+	summary := fmt.Sprintf("%s\n", summaryOfPost(post))
+	for _, old := range oldPosts {
+		summary += fmt.Sprintf("  of: %s\n", summaryOfPost(old))
 	}
-	return message
+	return summary
+}
+
+func summaryOfPost(post Post) string {
+	return fmt.Sprintf("feed=%s; subject=%s; id=%s", post.Feed.Id, post.Subject, post.Id)
 }
--- listeners_test.go
+++ listeners_test.go
@@ -25,17 +25,10 @@ func Test_consolePrinterListener_happy_path(t *testing.T) {
 	ConsolePrinterListener{}.OnDuplicates(postWithFeed, []Post{postWithFeed})
 }
 
-func Test_formatAsEmail(t*testing.T) {
-	post := Post{Id: "feed1-1", Subject: "feed1-1", Feed: &Feed{Id: "feed1"}}
-	old := []Post{
-		{Id: "feed2-1", Subject: "feed2-1", Feed: &Feed{Id: "feed2"}},
-		{Id: "feed3-1", Subject: "feed3-1", Feed: &Feed{Id: "feed3"}},
-	}
-
-	actual := formatAsEmail(post, old)
-	expected := "feed1: possible cross-post on: feed1-1 (feed1-1)\n"
-	expected += "  of: (feed2:) feed2-1 (feed2-1)\n"
-	expected += "  of: (feed3:) feed3-1 (feed3-1)\n"
+func Test_summaryOfPost(t*testing.T) {
+	post := Post{Id: "feed1-1", Subject: "sub1-1", Feed: &Feed{Id: "feed1"}}
+	actual := summaryOfPost(post)
+	expected := "feed=feed1; subject=sub1-1; id=feed1-1"
 
 	if actual != expected {
 		t.Fatalf("got: %s\nexpected: %s", actual, expected)
