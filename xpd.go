6877cbad02bd3eaf7d0f7d19db9d0931972b7ee9
Janos Gyerik <janos@kronos>
2016-10-22 22:50:14 +0200

    Move basic config checks higher, before parsing

--- xpd.go
+++ xpd.go
@@ -106,19 +106,19 @@ type Context struct {
 }
 
 func ParseContext(config *Config) (*Context, error) {
-	readers := parseReaders(config)
-
 	if len(config.Feeds) == 0 {
-		return nil, errors.New("configuration error: you must configure at least one feed")
+		return nil, errors.New("configuration error: configure at least one feed")
 	}
+
+	if len(config.Detectors) == 0 {
+		return nil, errors.New("configuration error: configure at least one detector")
+	}
+
+	readers := parseReaders(config)
 
 	detectors, err := parseDetectors(config.Detectors)
 	if err != nil {
 		return nil, err
-	}
-
-	if len(config.Detectors) == 0 {
-		return nil, errors.New("configuration error: you must configure at least one detector")
 	}
 
 	extraListeners, err := parseListeners(config.Listeners)
